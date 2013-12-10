af6faa13aed9170eefd87daef6196ef58d3e89d0
ercchy <erika@serox.si>
2013-12-10 16:03:23 +0100

    adding a script to remove symlinks

--- remove_link_pads.sh
+++ remove_link_pads.sh
@@ -5,7 +5,7 @@ while read line
 do
     name=$line
     let COUNTER=COUNTER+1
-    echo "$COUNTER. Padname: $name is getting a link"
+    echo "$COUNTER. Padname: $name is losing a link"
     rm $name
     echo "done with $name file"
     sleep 1
