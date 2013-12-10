1f63aa7aa6d48ecfe68efcd9414dcbd7930ecd34
ercchy <erika@serox.si>
2013-12-10 15:43:43 +0100

    removing unworking symlinks

---
+++ remove_link_pads.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+COUNTER=0
+while read line
+do
+    name=$line
+    let COUNTER=COUNTER+1
+    echo "$COUNTER. Padname: $name is getting a link"
+    rm $name
+    echo "done with $name file"
+    sleep 1
+done < $1
